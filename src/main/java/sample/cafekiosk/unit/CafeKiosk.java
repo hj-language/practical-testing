c0e507039d5b23d91f10eb223432d8639870d14a
Hyejin Kim <idgafff010@gmail.com>
2025-01-17 01:25:59 +0900

    feat: 키오스크 기본 기능 구현

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,12 +1,26 @@
 package sample.cafekiosk.unit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CafeKiosk {
+    private final List<Beverage> beverages = new ArrayList<>();
 
     public void add(Beverage beverage) {
-//        System.out.println(beverage.getName() + " 추가");
+        beverages.add(beverage);
+    }
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
     }
 
     public int calculateTotalPrice() {
-        return 0;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 }
