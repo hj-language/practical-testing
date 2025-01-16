0476da6bebd0066f58c29029fc2c4ca212627b22
Hyejin Kim <idgafff010@gmail.com>
2025-01-17 01:24:10 +0900

    feat: 키오스크 추가 및 프로토타입 구현

---
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -0,0 +1,12 @@
+package sample.cafekiosk.unit;
+
+public class CafeKiosk {
+
+    public void add(Beverage beverage) {
+//        System.out.println(beverage.getName() + " 추가");
+    }
+
+    public int calculateTotalPrice() {
+        return 0;
+    }
+}
