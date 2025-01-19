a0ac9e7a0b51325c58a771ae452d64c82198a9b4
Hyejin Kim <idgafff010@gmail.com>
2025-01-20 00:49:23 +0900

    test: CafeKiosk 자동화된 테스트 추가 - remove
    - hasSize(), isEmpty() 안 되는 문제: assertThat 메서드를 AssertionsForClassTypes 클래스 대신 AssertionsForInterfaceTypes 에서 찾아야 함

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -3,7 +3,8 @@ package sample.cafekiosk.unit;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 class CafeKioskTest {
 
@@ -21,9 +22,20 @@ class CafeKioskTest {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
-//        List<Beverage> beverages = cafeKiosk.getBeverages();
-//        assertThat(beverages).hasSize(1);
         assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().getFirst().getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 }
