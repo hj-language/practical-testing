0f0ec0d017daef68720d9d2684ed7351c9afc6e2
Hyejin Kim <idgafff010@gmail.com>
2025-01-20 00:41:58 +0900

    test: CafeKiosk 자동화된 테스트 추가
    - 강의에서는 assertThat().hasSize()가 가능했는데 되지 않아서 우선 주석 처리
    - 버전 차이 확인 필요

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -3,16 +3,27 @@ package sample.cafekiosk.unit;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CafeKioskTest {
+
+    @Test
+    void add_manual_test() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().getFirst().getName());
+    }
 
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
-        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
-        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().getFirst().getName());
+//        List<Beverage> beverages = cafeKiosk.getBeverages();
+//        assertThat(beverages).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages().getFirst().getName()).isEqualTo("아메리카노");
     }
 }
