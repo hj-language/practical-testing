c9ed9a833bb137aabba542c52f54e42cd06509b4
Hyejin Kim <idgafff010@gmail.com>
2025-01-22 02:35:39 +0900

    feat: Display 할 Product 목록 반환하는 비즈니스 로직 구현

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductSellingType.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductSellingType.java
@@ -3,6 +3,8 @@ package sample.cafekiosk.spring.domain.product;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
 public enum ProductSellingType {
@@ -11,4 +13,8 @@ public enum ProductSellingType {
     STOP_SELLING("판매 중지");
 
     private final String text;
+
+    public static List<ProductSellingType> forDisplay() {
+        return List.of(SELLING, HOLD);
+    }
 }
