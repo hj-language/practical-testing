f97f2f65afe566feb5a5d3ec40c855cfaa7166e0
Hyejin Kim <idgafff010@gmail.com>
2025-01-24 00:52:33 +0900

    test: OrderServiceTest 추가 - when 작성
    - 서비스의 createOrder() 결과를 확인하기 위해 응답 DTO를 OrderResponse를 추가하고, 메서드에서 해당 DTO를 반환하도록 변경한다.

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
@@ -33,6 +34,7 @@ class OrderServiceTest {
                 .build();
 
         // when
+        OrderResponse orderResponse = orderService.createOrder(request);
 
         // then
     }
