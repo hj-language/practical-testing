8ef46016ef59121e14dce70cdd73f4efc9dc3673
Hyejin Kim <idgafff010@gmail.com>
2025-02-04 01:56:38 +0900

    fix: 기본 생성자 추가

--- src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
@@ -2,12 +2,14 @@ package sample.cafekiosk.spring.api.controller.order.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class OrderCreateRequest {
-    private final List<String> productNumbers;
+    private List<String> productNumbers;
 
     @Builder
     public OrderCreateRequest(List<String> productNumbers) {
